ae771cf7ae904a0367eb14f4459151a5f3f97eb7
rouslan <rouslanboyko.webdeveloper@gmail.com>
2022-01-18 10:48:21 +0100

    User Registration with password encryption

--- models/User.php
+++ models/User.php
@@ -2,19 +2,32 @@
 
 namespace app\models;
 
+use app\core\DbModel;
 use app\core\Model;
 
-class RegisterModel extends Model
+class User extends DbModel
 {
+    const STATUS_INACTIVE = 0;
+    const STATUS_ACTIVE = 1;
+    const STATUS_DELETED = 2;
+
     public string $firstname = '';
     public string $lastname = '';
+    public int $status = self::STATUS_INACTIVE;
     public string $email = '';
     public string $password = '';
     public string $confirmPassword = '';
 
-    public function register()
+    public function tableName(): string
     {
-        echo 'Creating new user';
+        return 'users';
+    }
+
+    public function save()
+    {
+        $this->status = self::STATUS_INACTIVE;
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
+        return parent::save();
     }
 
     public function rules(): array
@@ -27,5 +40,10 @@ class RegisterModel extends Model
             'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
 
         ];
+    }
+
+    public function attributes(): array
+    {
+        return ['firstname', 'lastname', 'email', 'password', 'status'];
     }
 }
